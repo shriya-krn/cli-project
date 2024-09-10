80d670fe9a6439a026a248c48536f891a4dcc7cb
Shriya Karn <shriyakarn6@gmail.com>
2024-09-10 11:37:25 +0545

    Add cli project

---
+++ user.py
@@ -0,0 +1,135 @@
+def create_table(conn):
+    CREATE_USERS_TABLE_QUERY = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name CHAR(255) NOT NULL,
+            last_name CHAR(255) NOT NULL,
+            company_name CHAR(255) NOT NULL,
+            address CHAR(255) NOT NULL,
+            city CHAR(255) NOT NULL,
+            county CHAR(255) NOT NULL,
+            state CHAR(255) NOT NULL,
+            zip REAL NOT NULL,
+            phone1 CHAR(255) NOT NULL,
+            phone2 CHAR(255),
+            email CHAR(255) NOT NULL,
+            web text
+        );
+    """
+    cur = conn.cursor()
+    cur.execute(CREATE_USERS_TABLE_QUERY)
+    print("User table was created successfully."
+          import sqlite3
+
+
+def create_connection():
+    try:
+        con = sqlite3.connect("users.sqlite3")
+        return con
+    except Exception as e:
+        print(e)
+
+
+INPUT_STRING = """
+Enter the option: 
+    1. CREATE TABLE
+    2. DUMP users from csv INTO users TABLE
+    3. ADD new user INTO users TABLE
+    4. QUERY all users from TABLE
+    5. QUERY user by id from TABLE
+    6. QUERY specified no. of records from TABLE
+    7. DELETE all users
+    8. DELETE user by id
+    9. UPDATE user
+    10. Press any key to EXIT
+"""
+
+
+def create_table(conn):
+    CREATE_USERS_TABLE_QUERY = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name CHAR(255) NOT NULL,
+            last_name CHAR(255) NOT NULL,
+            company_name CHAR(255) NOT NULL,
+            address CHAR(255) NOT NULL,
+            city CHAR(255) NOT NULL,
+            county CHAR(255) NOT NULL,
+            state CHAR(255) NOT NULL,
+            zip REAL NOT NULL,
+            phone1 CHAR(255) NOT NULL,
+            phone2 CHAR(255),
+            email CHAR(255) NOT NULL,
+            web text
+        );
+    """
+    cur = conn.cursor()
+    cur.execute(CREATE_USERS_TABLE_QUERY)
+    print("User table was created successfully.")
+
+
+import csv
+
+
+def read_csv():
+    users = []
+    with open("sample_users.csv", "r") as f:
+        data = csv.reader(f)
+        for user in data:
+            users.append(tuple(user))
+
+    return users[1:]
+
+
+def insert_users(con, users):
+    user_add_query = """
+        INSERT INTO users
+        (
+            first_name,
+            last_name,
+            company_name,
+            address,
+            city,
+            county,
+            state,
+            zip,
+            phone1,
+            phone2,
+            email,
+            web
+        )
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
+    cur = con.cursor()
+    cur.executemany(user_add_query, users)
+    con.commit()
+    print(f"{len(users)} users were imported successfully.")
+
+
+def main():
+    con = create_connection()
+    user_input = input(INPUT_STRING)
+    if user_input == "1":
+        create_table(con)
+    elif user_input == "2":
+        users = read_csv()
+        insert_users(con, users)
+    elif user_input == "3":
+        pass
+    elif user_input == "4":
+        pass
+    elif user_input == "5":
+        pass
+    elif user_input == "6":
+        pass
+    elif user_input == "7":
+        pass
+    elif user_input == "8":
+        pass
+    elif user_input == "9":
+        pass
+    elif user_input == "10":
+        pass
+
+
+main()
